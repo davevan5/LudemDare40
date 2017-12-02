31b219ab8449b8ec22243401bd83e673292d6748
Sekhat Temporus <sekhat@manalightgames.com>
2017-12-02 13:39:06 +0000

    Fix sticky jump bug

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -38,21 +38,42 @@ const max_horizontal_speed = 300;
 
 var left_floor_raycast
 var right_floor_raycast
+var left_ceiling_raycast
+var center_ceiling_raycast
+var right_ceiling_raycast
+
 var animation_player
+var collision_shape
 
 func _ready():
 	animation_player = get_node("Player1Sprite/animation")
+	collision_shape = get_node("CollisionShape2D")
 	setup_raycasts()
 	set_fixed_process(true)
 	
 func setup_raycasts():
-	left_floor_raycast = get_node("LeftFloorRaycast")
-	left_floor_raycast.add_exception(self)
+	left_floor_raycast = get_node("LeftFloorRaycast")	
 	right_floor_raycast = get_node("RightFloorRaycast")
+	
+	left_floor_raycast.add_exception(self)
 	right_floor_raycast.add_exception(self)
+	
+	left_ceiling_raycast = get_node("RightCeilingRaycast")
+	center_ceiling_raycast = get_node("CenterCeilingRaycast")
+	right_ceiling_raycast = get_node("RightCeilingRaycast")
+	
+	left_ceiling_raycast.add_exception(self)
+	center_ceiling_raycast.add_exception(self)
+	right_ceiling_raycast.add_exception(self)
+	
 
 func is_on_ground():
 	return left_floor_raycast.is_colliding() || right_floor_raycast.is_colliding()
+	
+func is_touching_ceiling():
+	return left_ceiling_raycast.is_colliding() \
+		|| center_ceiling_raycast.is_colliding() \
+		|| right_ceiling_raycast.is_colliding()
 
 func calculate_horizontal_velocity_adjustment(delta, acceleration):
 	var result = Vector2(0, 0)
@@ -68,6 +89,18 @@ func calculate_horizontal_velocity_adjustment(delta, acceleration):
 func _fixed_process(delta):
 	var velocity = get_linear_velocity()
 	
+	if state == States.JUMPING:
+		jump_impulse -= jump_impulse_decay * delta
+		
+		# If we run out of jump then that means we must begin falling
+		if jump_impulse < 0 || !Input.is_action_pressed(action_jump) || is_touching_ceiling():
+			jump_impulse = 0
+			state = States.FALLING
+		else:
+			velocity.y = -jump_impulse * delta
+		
+		velocity += calculate_horizontal_velocity_adjustment(delta, air_acceleration)
+	
 	if state == States.GROUND:
 		if Input.is_action_pressed(action_jump):
 			state = States.JUMPING
@@ -77,17 +110,6 @@ func _fixed_process(delta):
 			
 		if !is_on_ground():
 			state = States.FALLING
-
-	if state == States.JUMPING:
-		jump_impulse -= jump_impulse_decay * delta
-		# If we run out of jump then that means we must begin falling
-		if jump_impulse < 0 || !Input.is_action_pressed(action_jump):
-			jump_impulse = 0
-			state = States.FALLING
-		else:
-			velocity.y = -jump_impulse * delta
-		
-		velocity += calculate_horizontal_velocity_adjustment(delta, air_acceleration)
 	
 	if state == States.FALLING:
 		if is_on_ground():
